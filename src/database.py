e4f7d7526b9f07c730d5e5e327fb9bf91f201f3b
Cyrus <z5207901@ad.unsw.edu.au>
2020-10-02 00:09:29 +1000

    Adding database.py

---
+++ src/database.py
@@ -0,0 +1,72 @@
+# To be put into iteration 1
+
+# User
+user = {}
+# user = {"u_id" : 1, "email" : "a", "name_first": "kelly", "name_last": "zhou", "handle_str": "kellyzhou"}
+# The general structure of user is summarised below
+# user = {
+#   'u_id': XX
+#   'email' = XX
+#   'name_first' = XX
+#   'name_last' = XX
+#   'handle_str' = XX
+# }
+# the auth register file in auth aims to pass in a user to append to users
+
+# Users
+users = []
+# Users contain a list of user
+# An example is as below:
+# [
+#   {
+#       'u_id': 100,
+#       'email': 'apple@gmail.com',
+#       'name_first': 'John',
+#       'name_last': 'Smith',
+#       'handle_str': 'herecomesjohnny'
+#   },
+#   {
+#       'u_id': 300,
+#       'email': 'Bpple@gmail.com',
+#       'name_first': 'Cohn',
+#       'name_last': 'Dmith',
+#       'handle_str': 'byeerecomesjohnny'
+#   }
+# ]
+
+# Message
+message = {}
+
+# Messages
+messages = []
+
+# Channel
+channel = {}
+
+# Channels
+channels = []
+
+# Member
+member = {}
+
+# Members
+members = []
+
+# add functions below if needed
+def clear():
+    global user
+    user.clear()
+    global users
+    users.clear()
+    global message
+    message.clear()
+    global messages
+    messages.clear()
+    global channel
+    channel.clear()
+    global channels
+    channels.clear()
+    global member
+    member.clear()
+    global members
+    members.clear()
