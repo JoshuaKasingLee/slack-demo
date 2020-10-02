b771830c2da0ce03b34d4bf6619c98d58e625fd8
Joshua Lee <z5209696@ad.unsw.edu.au>
2020-10-02 19:31:37 +1000

    join_3 commit

--- src/channel_join.py
+++ src/channel_join.py
@@ -1,3 +1,24 @@
+import database_edited as db
+from error import InputError
+from error import AccessError
+
 def channel_join(token, channel_id):
-    return {
-    }
+
+    u_id = token
+    if_in = 0
+    try :
+        for member in db.channels_and_members[channel_id][1]:
+            if member['u_id'] == u_id:
+                if_in = 1
+    except:
+        raise InputError
+    if if_in != 1 :
+        return {} 
+    '''dont know how to check if private for a channel and also global
+        somehow access error is meant to fit here too
+    '''
+    elif if_in == 0 :
+        member = {'u_id' : u_id}
+        member_copy = member.copy()
+        db.channels_and_members[channel_id][1].append(member_copy)
+    
