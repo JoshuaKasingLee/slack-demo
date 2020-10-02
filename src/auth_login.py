bdabd8bd3e0781c433cb4479b0d43ad80d2bd97f
Cyrus <z5207901@ad.unsw.edu.au>
2020-10-02 23:25:55 +1000

    editing auth_login

--- src/auth_login.py
+++ src/auth_login.py
@@ -1,4 +1,23 @@
+import re
+from error import InputError
+from database import master_users
+
 def auth_login(email, password):
+    
+    # check whether email is valid
+    # given regex function from: https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if not re.search(regex, email):
+        raise InputError(f"Error, {email} is invalid")
+
+    # check if a user is already logged on
+
+    # 
+
+
+
+
+
     return {
         'u_id': 1,
         'token': '12345',
