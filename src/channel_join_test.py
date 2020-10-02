edca79e6b5b96d2d121055d07e9352d69565ebfb
Joshua Lee <z5209696@ad.unsw.edu.au>
2020-10-02 18:53:10 +1000

    continued channel_join

--- src/channel_join_test.py
+++ src/channel_join_test.py
@@ -1,18 +1,29 @@
 import pytest
 import database
-from channel_invite import channel_invite
+from channel_join import channel_join
 import channels
 import auth
-import error
+from error import InputError
+from error import AccessError
 
 def test_join() :
     (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
     channel_id = channels.channels_create(token, "Channel1", True)
     channel_join(token, channel_id)
-    assert channels_and_members[f"{channel_id}"][1][0]['u_id'] == token
+    assert database.channels_and_members[channel_id][1][0]['u_id'] == token
     database.clear()
+
 def test_valid_channel() :
     (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
     channel_id = 999
     with pytest.raises(InputError) as e:
         assert channel_join(token, channel_id)
+    database.clear()
+
+def test_private_access() :
+    (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
+    channel_id = channels.channels_create(token, "Channel1", False)
+    channel_join(token, channel_id)
+    with pytest.raises(AccessError) as e:
+        assert channel_join(token, channel_id)
+    database.clear()
