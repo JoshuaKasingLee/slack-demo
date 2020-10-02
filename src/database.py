5ea0b003d416696bccb156be4e4822725cfb78ce
Kelly Zhou <z5215357@ad.unsw.edu.au>
2020-10-02 21:22:06 +1000

    updated database to include master_users variable

--- src/database.py
+++ src/database.py
@@ -1,5 +1,28 @@
 # To be put into iteration 1
 
+# (not given in spec) master users variables (contains ALL data relevant to ALL registered users)
+# should update token each time we login
+master_users = []
+# [
+#   {
+#       'u_id': 100,
+#       'email': 'apple@gmail.com',
+#       'name_first': 'John',
+#       'name_last': 'Smith',
+#       'handle_str': 'herecomesjohnny'
+#       'password': ilikecats
+#       'token': 100
+#   },
+#   {
+#       'u_id': 300,
+#       'email': 'Bpple@gmail.com',
+#       'name_first': 'Cohn',
+#       'name_last': 'Dmith',
+#       'handle_str': 'byeerecomesjohnny'
+#       'password': ilikedogs
+#       'token': 300
+#   }
+
 # User
 user = {}
 # user = {"u_id" : 1, "email" : "a", "name_first": "kelly", "name_last": "zhou", "handle_str": "kellyzhou"}
