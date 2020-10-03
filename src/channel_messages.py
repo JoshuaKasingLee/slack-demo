ae99428163c8f2630a5b817d69837550ba295457
z5308289 <z5308289@cse.unsw.edu.au>
2020-10-04 04:18:38 +1100

    fixing some issues, updating tests

--- src/channel_messages.py
+++ src/channel_messages.py
@@ -4,8 +4,12 @@ from error import AccessError
 
 '''
 # TO PUT IN DATABASE
-channels_and_messages = { channel_id: messages, channel_id2: messages2, ... }
-channels_and_messages = { 1: [ { 'message_id': 1, 'u_id': 1, 'message' = 'whtever the fuck', 'time_created' = 1111111111 }, { 'message_id': 2, ... } ... ], 2: ... }
+#channels_and_messages = { channel_id: messages, channel_id2: messages2, ... }
+#channels_and_messages = { 1: [ { 'message_id': 1, 'u_id': 1, 'message' = 'whtever the fuck', 'time_created' = 1111111111 }, { 'message_id': 2, ... } ... ], 2: ... }
+channels_and_messages = {}
+
+assumptions: we can only return 50 messages. between start and start+50 there are either 51 or 49 (inclusive or exclusive).
+i will return between start and start+49 inclusive, with end index = start+50 (or whatever it happens to be, depending on message count).
 '''
 
 def channel_messages(token, channel_id, start):
@@ -23,14 +27,14 @@ def channel_messages(token, channel_id, start):
             if member['u_id'] == u_id:
                 access = 1
     except:
-        raise InputError # channel doesn't exist
+        raise AccessError # channel doesn't exist
     if access == 0:
         raise AccessError # user is not a member
 
     messages = db.channels_and_messages[channel_id]
 
     ## check 'start' isn't greater than total # of messages
-    message_count = max(messages, key = int)
+    message_max = len(messages)
     if start > message_max:
         raise InputError
 
@@ -39,7 +43,10 @@ def channel_messages(token, channel_id, start):
     while (current <= message_max) and (current < start + 50):
         messages_return.append(messages[current])
         current += 1
-    current -= 1 # because we went one too far at the end
+    
+    # if we have reached the final message, return -1
+    if current > message_max:
+        current = -1
 
     return { 'messages': messages_return, 'start': start, 'end': current, }
 '''
--- src/channel_messages_test.py
+++ src/channel_messages_test.py
@@ -2,13 +2,14 @@ import pytest
 import database
 import channel_messages
 import error
+## idk what to put here
 
-def test_no_messages():
+def test_one_message():
     (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
     channel_id = channels.channels_create(token, "channel1", True)
     db.channels_and_messages[channel_id] = [
             {
-                'message_id': 1,
+                'message_id': 0,
                 'u_id': 1,
                 'message': 'Hello world',
                 'time_created': 1582426789,
@@ -17,12 +18,163 @@ def test_no_messages():
     assert(channel_messages(token, channel_id, 0) == {
         'messages': [
             {
-                'message_id': 1,
+                'message_id': 0,
                 'u_id': 1,
                 'message': 'Hello world',
                 'time_created': 1582426789,
             }
         ],
         'start': 0,
-        'end': 0,
-    })
+        'end': -1,
+    }
+    )
+clear()
+
+def test_two_messages():
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = [
+            {
+                'message_id': 0,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426789,
+            }, {
+                'message_id': 1,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788,
+            }
+
+        ]
+    assert(channel_messages(token, channel_id, 0) == {
+        'messages': [
+            {
+                'message_id': 0,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426789,
+            }, {
+                'message_id': 1,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788,
+            }
+        ],
+        'start': 0,
+        'end': -1,
+    }
+    )
+clear()
+
+def test_two_messages_start_1():
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = [
+            {
+                'message_id': 0,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426789,
+            }, {
+                'message_id': 1,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788,
+            }, {
+                'message_id': 2,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426787,
+            }
+
+        ]
+    assert(channel_messages(token, channel_id, 1) == {
+        'messages': [
+            {
+                'message_id': 1,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788,
+            }, {
+                'message_id': 2,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426787,
+            }
+        ],
+        'start': 1,
+        'end': -1,
+    }
+    )
+clear()
+
+def test_return_index(): # so it returns an index that isn't -1
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    channel_id = channels.channels_create(token, "channel1", True)
+    messages_list = []
+    messages_list_50 = []
+    for i in range(52):
+        messages_list.append(
+            {
+                'message_id': i,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788 - i,
+            }
+        )
+    for i in range(49):
+        messages_list_50.append(
+            {
+                'message_id': i,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426788 - i,
+            }
+        )
+    db.channels_and_messages[channel_id] = messages_list
+    assert(channel_messages(token, channel_id, 0) == {
+        'messages': messages_list_50,
+        'start': 0,
+        'end': 50,
+    }
+    )
+
+
+def test_no_messages(): # raise InputError
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = []
+    with pytest.raises(InputError):
+        channel_messages(token, channel_id, 1)
+
+def test_large_index(): # index too large, raise InputError
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = [
+            {
+                'message_id': 0,
+                'u_id': 1,
+                'message': 'Hello world',
+                'time_created': 1582426789,
+            }
+        ]
+    with pytest.raises(InputError):
+        channel_messages(token, channel_id, 10)
+
+def test_invalid_token(): # invalid token - AccessError
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = [
+    with pytest.raises(AccessError):
+        channel_messages('heyheyhey', channel_id, 0)
+
+def test_missing_channel(): # invalid channel_id - AccessError
+    (u_id, token) = auth.auth_register("user1@gmail.com", "password", "user1", "lastname1")
+    with pytest.raises(AccessError):
+        channel_messages(token, channel_id, 0)
+
+def test_missing_user(): # user doesn't exist - AccessError
+    channel_id = channels.channels_create(token, "channel1", True)
+    db.channels_and_messages[channel_id] = [
+    with pytest.raises(AccessError):
+        channel_messages(22, channel_id, 0)
