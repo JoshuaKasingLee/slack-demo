53337334b7fe645e1ada740b36ad16e77f5eb4e8
z5308289 <z5308289@cse.unsw.edu.au>
2020-10-03 17:19:57 +1000

    revised channels_create

--- src/channels_create.py
+++ src/channels_create.py
@@ -15,24 +15,19 @@ def channels_create(token, name, is_public):
         u_id = int(token)
     except:
         raise AccessError
-    ''' 
-    ## check user exists
+    
+    ## check user exists (not currently working, idk)
     user_exists = 0
     for user in db.users:
         if user['u_id'] == u_id:
             user_exists = 1
-    if user_exists == 0:
+    if user_exists != 1:
         raise AccessError   
-    '''
+    
     ## check valid name
     if len(name) > 20 or len(name) == 0:
         raise InputError
-    '''
-    ## check name not already taken
-    for channel in db.channels:
-        if channel['name'] == name:
-            raise InputError
-    '''
+
     ## add channel details
     channel_id = len(db.channels) + 1
 
@@ -41,13 +36,17 @@ def channels_create(token, name, is_public):
     channel['name'] = name
 
     ## add to various lists
-
-    db.channels_and_members[channel_id] = [[],[],]
-    db.channels.append(channel.copy())
+    # extract member details from user
+    name_first = users[u_id]['name_first']
+    name_last = users[u_id]['name_last']
+    member = { u_id, name_first, name_last }
+    members = [member]
+    db.channels_and_members[channel_id] = [members, members]
+    db.channels.append(channel)
     if is_public == True:
-        db.public_channels.append(channel.copy())
+        db.public_channels.append(channel)
     elif is_public == False:
-        db.private_channels.append(channel.copy())
+        db.private_channels.append(channel)
 
     ## add creator as owner
     channel_join(token, channel_id)
@@ -55,6 +54,3 @@ def channels_create(token, name, is_public):
     return {
         'channel_id': channel_id, # is this right?
     }
-
-user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
-print(channels_create(user1_token, 'exceptionalll', True))
