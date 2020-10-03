96fe51894c1dde4815965ffe7c56f007305f94af
z5308289 <z5308289@cse.unsw.edu.au>
2020-10-03 16:19:19 +1000

    commented in tests

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -83,3 +83,52 @@ def test_invalid_user():
         channels_listall(token)
         
 db.clear()    
+
+'''
+## tests which don't work
+
+# there are no channels
+def test_no_channels():
+    user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
+    assert(channels.channels_listall(user1_token) == {'channels': [],}
+
+db.clear()
+
+# there is one public channel
+def test_no_memberships():
+    user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
+    channel1 = channels.channels_create(user1_token, 'channel1', True)
+    assert(channels.channels_listall(user1_token) == {'channels': [ {'channel_id': 1, 'name': 'channel1' }],}
+db.clear()
+
+# there is one private channel
+def test_no_memberships():
+    user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
+    channel1 = channels.channels_create(user1_token, 'channel1', False)
+    assert(channels.channels_listall(user1_token) == {'channels': [ {'channel_id': 1, 'name': 'channel1' }],}
+db.clear()
+    
+# there are two channels
+def test_all_memberships():
+    user1_token = auth.auth_register('user@example.com', 'password', 'user1', 'name')['token']
+    channel1 = channels.channels_create(user1_token, 'channel1', True)
+    channel2 = channels.channels_create(user1_token, 'channel2', False)
+    assert(channels.channels_listall(user1_token) == {'channels': [ {'channel_id': 1, 'name': 'channel1' }, { 'channel_id': 2, 'name': 'channel2' }],}
+db.clear()
+
+# INVALID TOKEN
+def test_invalid_token():
+    user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
+    channel1 = channels.channels_create(user1_token, 'channel1', True)
+    with pytest.raises(AccessError) as e: # what does this e mean
+        channels_listall('bad token')
+db.clear()
+
+# user does not exist
+def test_missing_user():
+    user1_token = auth.auth_register('user1@example.com', 'password', 'user1', 'name')['token']
+    channel1 = channels.channels_create(user1_token, 'channel1', True)
+    with pytest.raises(AccessError) as e:
+        channels_listall(996)
+db.clear()
+'''
