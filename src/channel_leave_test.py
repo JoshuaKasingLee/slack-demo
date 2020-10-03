1cb8fcca8a1052b01c54a9df1c1d758d07a981db
Joshua Lee <z5209696@ad.unsw.edu.au>
2020-10-03 11:52:17 +1000

    chanel leave tests

--- src/channel_leave_test.py
+++ src/channel_leave_test.py
@@ -1,33 +1,35 @@
 import pytest
 import database
+from channel_leave import channel_leave
 from channel_join import channel_join
 import channels
 import auth
 from error import InputError
 from error import AccessError
 
-def test_join() :
+def test_leave() :
     (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
     channel_id = channels.channels_create(token, "Channel1", True)
     channel_join(token, channel_id)
+    channel_leave(token, channel_id)
     (name, owner_members, all_members)  = channel_details(token, channel_id)
     is_in = 0
     for member in all_members:
         if member['u_id'] == u_id:
             is_in = 1
-    assert (is_in == 1)
+    assert (is_in == 0)
     database.clear()
 
 def test_valid_channel() :
     (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
     channel_id = 999
     with pytest.raises(InputError) as e:
-        channel_join(token, channel_id)
+        channel_leave(token, channel_id)
     database.clear()
 
-def test_private_access() :
+def test_not_a_channel_member() :
     (u_id, token) = auth.auth_register("email@gmail.com", "password", "Joshua", "Lee")
-    channel_id = channels.channels_create(token, "Channel1", False)
+    channel_id = channels.channels_create(token, "Channel1", True)
     with pytest.raises(AccessError) as e:
-        channel_join(token, channel_id)
+        channel_leave(token, channel_id)
     database.clear()
