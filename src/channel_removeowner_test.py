e2a8cbf072e2a629b518a6a752c7447327069ab7
z5308289 <z5308289@cse.unsw.edu.au>
2020-10-17 01:56:51 +1100

    fixed and re-added previously broken tests

--- src/channel_removeowner_test.py
+++ src/channel_removeowner_test.py
@@ -5,7 +5,10 @@ import channels
 import auth
 from error import InputError, AccessError
 
-
+#need to test a few things: 
+# valid channel (input error), u_id is not an owner (input error).
+# access error if token is not global owner OR owner of channel
+# regular testing: token is owner and u_id is owner
 
 def test_valid_channel() :
     clear()
@@ -42,3 +45,29 @@ def test_not_global_or_local_owner() :
     with pytest.raises(AccessError) as e:
         channel_removeowner(token_2, channel_id, u_id)
     clear()   
+
+
+def test_global_but_not_local_owner() :
+    clear()
+    user = auth.auth_register("test1@gmail.com", "password", "John", "Smith")
+    u_id = user['u_id']
+    token = user['token']
+    user_2 = auth.auth_register("email2@gmail.com", "passwords", "Johns", "Smiths")
+    u_id_2 = user_2['u_id']
+    token_2 = user_2['token']   
+    channel_id = channels.channels_create(token_2, "Channel1", True)['channel_id']
+    channel_join(token, channel_id) # didn't work b4 bc global was not member of channel
+    channel_removeowner(token, channel_id, u_id_2)
+    owner_members = channel_details(token, channel_id)['owner_members']
+    all_members = channel_details(token, channel_id)['all_members']
+    is_in_owner = 0
+    for member in owner_members:
+        if member['u_id'] == u_id_2:
+            is_in_owner = 1
+    assert (is_in_owner == 0)
+    is_in_all = 0
+    for member in all_members:
+        if member['u_id'] == u_id_2:
+            is_in_all = 1
+    assert (is_in_all == 1)
+    clear()
