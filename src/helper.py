17cd85f29222a42c51a788d31073ab31f97763a4
Andreea <a.vissarion@student.unsw.edu.au>
2020-10-22 20:38:07 +1100

    src/helper.py - modified regex to evade warnings

--- src/helper.py
+++ src/helper.py
@@ -7,7 +7,7 @@ import database
 
 def validate_email(email):
     '''check valid email, regex function from: https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/'''
-    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if not re.search(regex, email):
         raise InputError(f"Error, {email} is invalid")
 
