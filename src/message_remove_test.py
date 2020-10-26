f51dfe2e21b27a6dc5d8beac285b4e121052907a
Joshua Lee <z5209696@ad.unsw.edu.au>
2020-10-26 15:19:02 +1100

    added extra message_remove tests

--- src/message_remove_test.py
+++ src/message_remove_test.py
@@ -2,6 +2,8 @@ import pytest
 from message import message_send, message_remove
 import auth
 import channels
+import message
+import channel
 from error import AccessError
 from error import InputError
 from other import clear
@@ -11,6 +13,7 @@ from other import clear
 
 # Test message no longer exists
 def test_message_already_deleted():
+    clear()
     user_token = auth.auth_register("user@gmail.com", "password", "John", "Smith")['token']
     channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
     message_to_send = 'Hi!'
@@ -22,6 +25,7 @@ def test_message_already_deleted():
 
 # Test message never existed
 def test_message_never_existed():
+    clear()
     user_token = auth.auth_register("user@gmail.com", "password", "John", "Smith")['token']
     channels.channels_create(user_token, "Test Channel", True)['channel_id']
     message_id = 15
@@ -31,6 +35,7 @@ def test_message_never_existed():
 
 # Test user is not an owner of the channel
 def test_user_not_owner():
+    clear()
     user_token = auth.auth_register("user@gmail.com", "password", "John", "Smith")['token']
     channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
     message_to_send = 'Hi!'
@@ -39,3 +44,16 @@ def test_user_not_owner():
     with pytest.raises(AccessError):
         message_remove(user_token, message_id)
     clear()
+
+def test_working_removal():
+    clear()
+    user_token = auth.auth_register("user@gmail.com", "password", "John", "Smith")['token']
+    channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
+    message_to_send = 'Hi!'
+    message_id = message_send(user_token, channel_id, message_to_send)['message_id']
+    message_remove(user_token, message_id)
+    data = channel.channel_messages(user_token, channel_id, 0)
+    assert (data['messages'] == [])
+    clear()
+
+
