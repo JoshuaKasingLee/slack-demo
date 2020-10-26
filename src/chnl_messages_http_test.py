2302ef508667b012a3c5e6fa1f602a100ddab615
Joshua Lee <z5209696@ad.unsw.edu.au>
2020-10-26 11:52:07 +1100

    edited all http files to src/server.py from server.py in url function

--- src/chnl_messages_http_test.py
+++ src/chnl_messages_http_test.py
@@ -18,7 +18,7 @@ data_in_1 = {
 @pytest.fixture
 def url():
     url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
+    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
     line = server.stderr.readline()
     local_url = url_re.match(line.decode())
     if local_url:
