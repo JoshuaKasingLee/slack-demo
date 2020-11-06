09edef92d6d450d448f9eaa9a15863109909ca07
Cyrus <z5207901@ad.unsw.edu.au>
2020-11-06 20:54:15 +1100

    /src/message_sendlater_test.py - Completed preliminary testing of function

--- src/message_sendlater_test.py
+++ src/message_sendlater_test.py
@@ -3,6 +3,8 @@ from message import message_sendlater
 import auth
 import channel
 import channels
+import time
+import threading
 from error import AccessError
 from error import InputError
 from other import clear
@@ -12,18 +14,48 @@ from other import clear
 def test_message_delay():
     clear()
     ## Insert Code
+    user = auth.auth_register("user@gmail.com", "password", "John", "Smith")
+    user_token = user['token']
+    u_id = user['u_id']
+    channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
+    message_to_send = 'Hi!'
+    curr_time = time.time()
+    time_sent = curr_time + 5
+    message_id = message_sendlater(user_token, channel_id, message_to_send, time_sent)
+    assert(len(channel.channel_messages(user_token, channel_id, 0)['messages']) == 0)
+    time.sleep(5)
+    assert(len(channel.channel_messages(user_token, channel_id, 0)['messages']) == 1)
     clear()
 
 # Test to make sure test cannot be sent into the past ~ Raise input error
 def test_message_to_past():
     clear()
     ## Insert Code
+    user = auth.auth_register("user@gmail.com", "password", "John", "Smith")
+    user_token = user['token']
+    u_id = user['u_id']
+    channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
+    message_to_send = 'Hi!'
+    curr_time = time.time()
+    time_sent = curr_time - 5
+    with pytest.raises(InputError):
+        message_sendlater(user_token, channel_id, message_to_send, time_sent)
     clear()
 
 # Check to make sure channel_id is valid
 def test_valid_channel():
     clear()
     ## Insert Code
+    user = auth.auth_register("user@gmail.com", "password", "John", "Smith")
+    user_token = user['token']
+    u_id = user['u_id']
+    channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
+    channel_id = channel_id + 1
+    message_to_send = 'Hi!'
+    curr_time = time.time()
+    time_sent = curr_time + 5
+    with pytest.raises(InputError):
+        message_sendlater(user_token, channel_id, message_to_send, time_sent)
     clear()
 
 # Check to make sure message length is > than 1000
@@ -32,8 +64,8 @@ def test_message_length():
     user_token = auth.auth_register("user@gmail.com", "password", "John", "Smith")['token']
     channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
     channel.channel_join(user_token, channel_id)
-    # For now assume time is an integer with the time_sent to be seconds into the future
-    time_sent = 30
+    curr_time = time.time()
+    time_sent = curr_time + 5
     message_to_send = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Na'
     with pytest.raises(InputError):
         message_sendlater(user_token, channel_id, message_to_send, time_sent)
@@ -43,10 +75,34 @@ def test_message_length():
 def test_user_in_channel():
     clear()
     ## Insert Code
+    user_1 = auth.auth_register("user@gmail.com", "password", "John", "Smith")
+    user_token_1 = user_1['token']
+    u_id_1 = user_1['u_id']
+    channel_id_1 = channels.channels_create(user_token_1, "Test Channel", True)['channel_id']
+    user_2 = auth.auth_register("user1@gmail.com", "password", "John", "Smith")
+    user_token_2 = user_2['token']
+    u_id_2 = user_2['u_id']
+    channel_id_2 = channels.channels_create(user_token_2, "Test2 Channel", True)['channel_id']
+    message_to_send = 'Hi!'
+    curr_time = time.time()
+    time_sent = curr_time + 5
+    with pytest.raises(AccessError):
+        message_sendlater(user_token_2, channel_id_1, message_to_send, time_sent)
     clear()
 
 # Make sure message is correct (payload is correct)
 def test_message_success():
     clear()
     ## Insert Code
+    user = auth.auth_register("user@gmail.com", "password", "John", "Smith")
+    user_token = user['token']
+    u_id = user['u_id']
+    channel_id = channels.channels_create(user_token, "Test Channel", True)['channel_id']
+    message_to_send = 'Hi!'
+    curr_time = time.time()
+    time_sent = curr_time + 5
+    message_id = message_sendlater(user_token, channel_id, message_to_send, time_sent)
+    assert(len(channel.channel_messages(user_token, channel_id, 0)['messages']) == 0)
+    time.sleep(5)
+    assert(channel.channel_messages(user_token, channel_id, 0)['messages'][message_id]['message'] == message_to_send)
     clear()
